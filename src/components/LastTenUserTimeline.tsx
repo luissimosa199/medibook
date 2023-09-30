003eb3aff54d79bc5c1f1524219715083524fb60
Luis <simosa37@gmail.com>
2023-09-30 12:20:49 -0300

    iframe added to profile timelines

--- src/components/LastTenUserTimeline.tsx
+++ src/components/LastTenUserTimeline.tsx
@@ -1,127 +1,192 @@
-import { InputItem, TimelineFormInputs } from '@/types'
-import formatDateString from '@/utils/formatDateString'
-import { CldImage } from 'next-cloudinary'
-import { FunctionComponent } from 'react'
-import { useQuery } from '@tanstack/react-query'
+import { InputItem, TimelineFormInputs } from "@/types";
+import formatDateString from "@/utils/formatDateString";
+import { CldImage } from "next-cloudinary";
+import { FunctionComponent } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { extractTimestamp, extractVideoId, isYtUrl } from "@/utils/isYtUrl";
+import IFrame from "./Iframe";
+import YouTubePlayer from "./YoutubePlayer";
 
 interface LastTenUserTimelineProps {
-    username: string
+  username: string;
 }
 
-const LastTenUserTimeline: FunctionComponent<LastTenUserTimelineProps> = ({ username }) => {
+const LastTenUserTimeline: FunctionComponent<LastTenUserTimelineProps> = ({
+  username,
+}) => {
+  const fetchUserTimelines = async () => {
+    const response = await fetch(
+      `/api/user/timelines/?username=${encodeURIComponent(username)}&page=0`,
+      {
+        method: "GET",
+      }
+    );
+    const data = await response.json();
+    return data;
+  };
 
-    const fetchUserTimelines = async () => {
-        const response = await fetch(`/api/user/timelines/?username=${encodeURIComponent(username)}&page=0`, {
-            method: 'GET'
-        });
-        const data = await response.json()
-        return data
-    }
+  const { data, isLoading, isError } = useQuery(
+    [username, "userTimelines"],
+    fetchUserTimelines
+  );
 
-    const { data, isLoading, isError } = useQuery([username, 'userTimelines'], fetchUserTimelines)
-
-    if (isLoading) {
-        return (
-            <div className="mt-4 bg-white p-6 rounded-lg shadow-md animate-pulse">
-                <ul className="divide-y divide-gray-200">
-                    {[...Array(3)].map((_, index) => (
-                        <li key={index} className="py-4 space-y-4">
-                            <div className="h-4 bg-gray-300 w-1/3"></div>
-
-                            <div className="h-6 bg-gray-300 w-2/3 my-2"></div>
-
-                            <div className="flex gap-2">
-                                <div className="w-32 h-32 bg-gray-300 rounded"></div>
-                                <div className="w-32 h-32 bg-gray-300 rounded"></div>
-                            </div>
-
-                            <div className="flex flex-col gap-2">
-                                <div className="p-4 bg-gray-300 rounded-lg"></div>
-                                <div className="p-4 bg-gray-300 rounded-lg"></div>
-                            </div>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-
-        )
-    }
-
-    if (isError) {
-        return (
-            <div>Error</div>
-        )
-    }
-
+  if (isLoading) {
     return (
-        <div className="mt-4 bg-white p-6 rounded-lg shadow-md">
-            {data && data.length > 0 ? (
-                <ul className="divide-y divide-gray-200">
-                    {data.map((e: TimelineFormInputs, idx: number) => (
-                        <li key={idx} className="py-4 space-y-4">
-                            <p className="text-lg text-gray-600">{formatDateString(e.createdAt)}</p>
+      <div className="mt-4 bg-white p-6 rounded-lg shadow-md animate-pulse">
+        <ul className="divide-y divide-gray-200">
+          {[...Array(3)].map((_, index) => (
+            <li
+              key={index}
+              className="py-4 space-y-4"
+            >
+              <div className="h-4 bg-gray-300 w-1/3"></div>
 
-                            {e.mainText && <p className="text-xl mb-4 font-semibold">{e.mainText}</p>}
+              <div className="h-6 bg-gray-300 w-2/3 my-2"></div>
 
-                            {e.photo && e.photo.length > 0 && (
-                                <div className="flex gap-2">
-                                    {e.photo.map((media: any, mediaIdx: number) => {
-                                        const isVideo = media.url.includes("/dahu3rii0/video/upload/") && media.url.endsWith(".mp4");
-                                        return (
-                                            <div key={mediaIdx} className="w-fit overflow-hidden rounded">
-                                                {isVideo ? (
-                                                    <video controls width="200" height="200" className="object-cover">
-                                                        <source src={media.url} type="video/mp4" />
-                                                        Your browser does not support the video tag.
-                                                    </video>
-                                                ) : (
-                                                    <CldImage src={media.url} alt={media.caption || 'Timeline Image'} width={200} height={200} />
-                                                )}
-                                            </div>
-                                        );
-                                    })}
-                                </div>
-                            )}
+              <div className="flex gap-2">
+                <div className="w-32 h-32 bg-gray-300 rounded"></div>
+                <div className="w-32 h-32 bg-gray-300 rounded"></div>
+              </div>
 
+              <div className="flex flex-col gap-2">
+                <div className="p-4 bg-gray-300 rounded-lg"></div>
+                <div className="p-4 bg-gray-300 rounded-lg"></div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 
-                            {e.links && e.links.length > 0 && (
-                                <div className="flex flex-col gap-2">
-                                    {e.links.map((link: any) => { // use 'any' type temporarily
+  if (isError) {
+    return <div>Error</div>;
+  }
 
-                                        // old cases
-                                        const isStringLink = typeof link === 'string';
-                                        const linkValue = isStringLink ? link : link.value;
-                                        const linkCaption = typeof link === 'object' && link.caption;
+  return (
+    <div className="mt-4 bg-white p-6 rounded-lg shadow-md">
+      {data && data.length > 0 ? (
+        <ul className="divide-y divide-gray-200">
+          {data.map((e: TimelineFormInputs, idx: number) => (
+            <li
+              key={idx}
+              className="py-4 space-y-4"
+            >
+              <p className="text-lg text-gray-600">
+                {formatDateString(e.createdAt)}
+              </p>
 
-                                        return (
-                                            <div key={linkValue} className="p-4 bg-gray-100 hover:bg-gray-200 rounded-lg transition-colors duration-300">
-                                                <a
-                                                    href={linkValue}
-                                                    target="_blank"
-                                                    rel="noopener noreferrer"
-                                                    className=" text-blue-600 hover:text-blue-800 font-medium break-words"
-                                                >
-                                                    {linkValue}
-                                                </a>
-                                                <p className="text-base text-gray-500 mt-2">
-                                                    {linkCaption}
-                                                </p>
-                                            </div>
-                                        );
-                                    })}
+              {e.mainText && (
+                <p className="text-xl mb-4 font-semibold">{e.mainText}</p>
+              )}
 
-                                </div>
-                            )}
+              {e.photo && e.photo.length > 0 && (
+                <div className="flex gap-2">
+                  {e.photo.map((media: any, mediaIdx: number) => {
+                    const isVideo =
+                      media.url.includes("/dahu3rii0/video/upload/") &&
+                      media.url.endsWith(".mp4");
+                    return (
+                      <div
+                        key={mediaIdx}
+                        className="w-fit overflow-hidden rounded"
+                      >
+                        {isVideo ? (
+                          <video
+                            controls
+                            width="200"
+                            height="200"
+                            className="object-cover"
+                          >
+                            <source
+                              src={media.url}
+                              type="video/mp4"
+                            />
+                            Your browser does not support the video tag.
+                          </video>
+                        ) : (
+                          <CldImage
+                            src={media.url}
+                            alt={media.caption || "Timeline Image"}
+                            width={200}
+                            height={200}
+                          />
+                        )}
+                      </div>
+                    );
+                  })}
+                </div>
+              )}
 
-                        </li>
-                    ))}
-                </ul>
-            ) : (
-                <p className="text-center text-gray-500 mt-4">No has realizado tu primera publicación</p>
-            )}
-        </div>
+              {e.links &&
+                e.links.length > 0 &&
+                e.links.map((e: string | InputItem, idx: number) => {
+                  let src: string;
+                  let caption: string | undefined;
 
-    )
-}
+                  if (typeof e === "object" && e.value) {
+                    src = e.value;
+                    caption = e.caption;
+                  } else if (typeof e === "string") {
+                    src = e;
+                    caption = undefined;
+                  } else {
+                    return null;
+                  }
 
-export default LastTenUserTimeline
+                  if (isYtUrl(src) && extractVideoId(src)) {
+                    const start = extractTimestamp(src);
+
+                    return (
+                      <div
+                        key={src}
+                        className="mt-4 max-w-[800px] w-full mx-auto bg-white"
+                      >
+                        <div className="">
+                          <YouTubePlayer
+                            videoId={extractVideoId(src) as string}
+                            h="500px"
+                            start={start}
+                          />
+                          {caption && (
+                            <p className="text-lg text-gray-500 mt-2 ml-2">
+                              {caption}
+                            </p>
+                          )}
+                        </div>
+                      </div>
+                    );
+                  }
+
+                  return (
+                    <div
+                      key={src}
+                      className="mt-4 max-w-[800px] w-full mx-auto bg-white"
+                    >
+                      <div className="">
+                        <IFrame
+                          src={src}
+                          h="800px"
+                        />
+                        {caption && (
+                          <p className="text-lg text-gray-500 mt-2 ml-2">
+                            {caption}
+                          </p>
+                        )}
+                      </div>
+                    </div>
+                  );
+                })}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p className="text-center text-gray-500 mt-4">
+          No has realizado tu primera publicación
+        </p>
+      )}
+    </div>
+  );
+};
+
+export default LastTenUserTimeline;
--- src/pages/pacientes/[id].tsx
+++ src/pages/pacientes/[id].tsx
@@ -193,144 +193,148 @@ const Patient: FunctionComponent<PatientePageProps> = ({ patientData }) => {
           {patientData?.name}
         </h1>
       </div>
-      <div className="flex flex-col justify-around items-center border rounded-lg py-6 md:p-6 bg-white shadow-lg">
-        <div className="min-h-[128px] flex flex-col items-center relative">
-          <ProfilePicture
-            type="pacientes"
-            userId={patientData?._id as string}
-          />
-          <div className="border-2 absolute bottom-0 left-0 bg-white h-12 w-12 rounded-full overflow-hidden flex justify-center">
-            <PhotoInput
-              handleUploadImages={handleChangeAvatar}
-              variant="small"
-              id="profilepicture"
-            />
-          </div>
-        </div>
-
-        <div className="flex flex-col space-y-4 my-6">
-          <div className="flex items-center space-x-4">
-            <FontAwesomeIcon
-              icon={faUser}
-              className="w-6 h-6 text-gray-600"
-            />
-            <p className="text-lg text-gray-800">{patientData?.details}</p>
-          </div>
-          <div className="flex items-center space-x-4">
-            <FontAwesomeIcon
-              icon={faPhone}
-              className="w-6 h-6 text-gray-600"
-            />
-            <p className="text-lg text-gray-800">{patientData?.tlf}</p>
-          </div>
-          <div className="flex items-center space-x-4">
-            <FontAwesomeIcon
-              icon={faEnvelope}
-              className="w-6 h-6 text-gray-600"
-            />
-            <p className="text-lg text-gray-800">{patientData?.email}</p>
-          </div>
-        </div>
-
-        <div>
-          <h2 className="text-2xl mt-2 font-semibold text-gray-800 border-b-2 pb-3">
-            Fotos:
-          </h2>
-          <UserPhotos
-            userId={patientData?._id as string}
-            queryKey={["patientPhotos", patientData?._id as string]}
-          />
-          <div className="w-24 mx-auto">
-            <PhotoInput
-              handleUploadImages={handleUploadImages}
-              id="patientphotos"
-              variant="small"
-            />
-          </div>
-          <div className="mt-4 space-y-4">
-            {newImages &&
-              newImages.map((e: string, index: number) => {
-                const isVideo = e.includes("data:video/mp4");
-                return (
-                  <div
-                    key={index}
-                    className="flex items-center gap-2 bg-gray-100 p-4 rounded-md"
-                  >
-                    <button
-                      onClick={handleDeleteImage(index)}
-                      className="bg-red-500 text-white p-2 w-8 h-8 rounded-full hover:bg-red-600 flex justify-center items-center transition duration-300"
-                    >
-                      <FontAwesomeIcon
-                        className="w-8"
-                        icon={faX}
-                      />
-                    </button>
-                    {isVideo ? (
-                      <video
-                        controls
-                        width="200"
-                        height="200"
-                        className="rounded mx-auto"
-                      >
-                        <source
-                          src={e}
-                          type="video/mp4"
-                        />
-                        Your browser does not support the video tag.
-                      </video>
-                    ) : (
-                      <Image
-                        src={e}
-                        alt=""
-                        width={200}
-                        height={200}
-                      />
-                    )}
-                  </div>
-                );
-              })}
-          </div>
-          {newImages.length > 0 && (
-            <button
-              type="button"
-              onClick={handleSubmit}
-              disabled={isUploading}
-              className="mt-4 py-2 px-4 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition duration-300"
-            >
-              {isUploading ? "Subiendo..." : "Subir"}
-            </button>
-          )}
-        </div>
-
-        <div className="w-full">
-          <div className="mt-2 border-b-2 pb-3">
-            <div className="flex justify-between px-2 md:px-0">
-              <h2 className="text-2xl font-semibold text-gray-800 ">
-                Historias:
-              </h2>
-              <button
-                className={`border-2 w-10 rounded p-2 ${
-                  addNewTimeline ? "bg-gray-200" : "bg-white"
-                } text-slate-600 transition`}
-                onClick={(e) => {
-                  e.preventDefault();
-                  setAddNewTimeline(!addNewTimeline);
-                }}
-              >
-                <FontAwesomeIcon icon={faPlus} />
-              </button>
-            </div>
-            {addNewTimeline && (
-              <PrimaryForm
-                patientData={{
-                  pacientId: patientData?._id as string,
-                  pacientName: patientData?.name as string,
-                }}
+      <div className="max-w-[850px] mx-auto">
+        <div className="flex flex-col justify-around items-center border rounded-lg py-6 md:p-6 bg-white shadow-lg">
+          <div className="flex flex-col items-center lg:flex-row gap-2">
+            <div className="min-h-[128px] flex flex-col items-center relative">
+              <ProfilePicture
+                type="pacientes"
+                userId={patientData?._id as string}
               />
+              <div className="border-2 absolute bottom-0 left-0 bg-white h-12 w-12 rounded-full overflow-hidden flex justify-center">
+                <PhotoInput
+                  handleUploadImages={handleChangeAvatar}
+                  variant="small"
+                  id="profilepicture"
+                />
+              </div>
+            </div>
+
+            <div className="flex flex-col space-y-4 my-6">
+              <div className="flex items-center space-x-4">
+                <FontAwesomeIcon
+                  icon={faUser}
+                  className="w-6 h-6 text-gray-600"
+                />
+                <p className="text-lg text-gray-800">{patientData?.details}</p>
+              </div>
+              <div className="flex items-center space-x-4">
+                <FontAwesomeIcon
+                  icon={faPhone}
+                  className="w-6 h-6 text-gray-600"
+                />
+                <p className="text-lg text-gray-800">{patientData?.tlf}</p>
+              </div>
+              <div className="flex items-center space-x-4">
+                <FontAwesomeIcon
+                  icon={faEnvelope}
+                  className="w-6 h-6 text-gray-600"
+                />
+                <p className="text-lg text-gray-800">{patientData?.email}</p>
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <h2 className="text-2xl mt-2 font-semibold text-gray-800 border-b-2 pb-3">
+              Fotos:
+            </h2>
+            <UserPhotos
+              userId={patientData?._id as string}
+              queryKey={["patientPhotos", patientData?._id as string]}
+            />
+            <div className="w-24 mx-auto">
+              <PhotoInput
+                handleUploadImages={handleUploadImages}
+                id="patientphotos"
+                variant="small"
+              />
+            </div>
+            <div className="mt-4 space-y-4">
+              {newImages &&
+                newImages.map((e: string, index: number) => {
+                  const isVideo = e.includes("data:video/mp4");
+                  return (
+                    <div
+                      key={index}
+                      className="flex items-center gap-2 bg-gray-100 p-4 rounded-md"
+                    >
+                      <button
+                        onClick={handleDeleteImage(index)}
+                        className="bg-red-500 text-white p-2 w-8 h-8 rounded-full hover:bg-red-600 flex justify-center items-center transition duration-300"
+                      >
+                        <FontAwesomeIcon
+                          className="w-8"
+                          icon={faX}
+                        />
+                      </button>
+                      {isVideo ? (
+                        <video
+                          controls
+                          width="200"
+                          height="200"
+                          className="rounded mx-auto"
+                        >
+                          <source
+                            src={e}
+                            type="video/mp4"
+                          />
+                          Your browser does not support the video tag.
+                        </video>
+                      ) : (
+                        <Image
+                          src={e}
+                          alt=""
+                          width={200}
+                          height={200}
+                        />
+                      )}
+                    </div>
+                  );
+                })}
+            </div>
+            {newImages.length > 0 && (
+              <button
+                type="button"
+                onClick={handleSubmit}
+                disabled={isUploading}
+                className="mt-4 py-2 px-4 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition duration-300"
+              >
+                {isUploading ? "Subiendo..." : "Subir"}
+              </button>
             )}
           </div>
-          <div>
-            <PatientTimelines userId={patientData?._id as string} />
+
+          <div className="w-full">
+            <div className="mt-2 border-b-2 pb-3">
+              <div className="flex justify-between px-2 md:px-0">
+                <h2 className="text-2xl font-semibold text-gray-800 ">
+                  Historias:
+                </h2>
+                <button
+                  className={`border-2 w-10 rounded p-2 ${
+                    addNewTimeline ? "bg-gray-200" : "bg-white"
+                  } text-slate-600 transition`}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setAddNewTimeline(!addNewTimeline);
+                  }}
+                >
+                  <FontAwesomeIcon icon={faPlus} />
+                </button>
+              </div>
+              {addNewTimeline && (
+                <PrimaryForm
+                  patientData={{
+                    pacientId: patientData?._id as string,
+                    pacientName: patientData?.name as string,
+                  }}
+                />
+              )}
+            </div>
+            <div>
+              <PatientTimelines userId={patientData?._id as string} />
+            </div>
           </div>
         </div>
       </div>
