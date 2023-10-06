e1cdecc65de0a3d834020e95e4aa1a37e9b256eb
Luis <simosa37@gmail.com>
2023-10-06 14:24:43 -0300

    ad switch added

--- src/components/ProfileButtonsPanel.tsx
+++ src/components/ProfileButtonsPanel.tsx
@@ -1,16 +1,39 @@
-import { IconDefinition, faChartBar, faDollarSign, faLifeRing, faStethoscope, faUsers, faVideo } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import Link from 'next/link'
-import React, { FunctionComponent } from 'react'
+import {
+  // IconDefinition,
+  // faChartBar,
+  // faDollarSign,
+  // faLifeRing,
+  faStethoscope,
+  faUsers,
+  faVideo,
+} from "@fortawesome/free-solid-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
+import React, { FunctionComponent } from "react";
 
 const buttons = [
-    { icon: faStethoscope, href: "videocall", name: "Iniciar consulta", color: 'bg-sky-400' },
-    { icon: faUsers, href: "pacientes", name: "Pacientes", color: 'bg-emerald-400' },
-    // { icon: faLifeRing, href: "soporte", name: "Soporte", color: 'bg-yellow-400' },
-    { icon: faVideo, href: "pacientes", name: "Videollamadas", color: 'bg-violet-400' },
-    // { icon: faDollarSign, href: "promo", name: "Promoción", color: 'bg-green-400' },
-    // { icon: faChartBar, href: "estadisticas", name: "Ver estadísticas", color: 'bg-teal-400' },
-]
+  {
+    icon: faStethoscope,
+    href: "videocall",
+    name: "Iniciar consulta",
+    color: "bg-sky-400",
+  },
+  {
+    icon: faUsers,
+    href: "pacientes",
+    name: "Pacientes",
+    color: "bg-emerald-400",
+  },
+  // { icon: faLifeRing, href: "soporte", name: "Soporte", color: 'bg-yellow-400' },
+  {
+    icon: faVideo,
+    href: "pacientes",
+    name: "Videollamadas",
+    color: "bg-violet-400",
+  },
+  // { icon: faDollarSign, href: "promo", name: "Promoción", color: 'bg-green-400' },
+  // { icon: faChartBar, href: "estadisticas", name: "Ver estadísticas", color: 'bg-teal-400' },
+];
 
 // interface ProfileButtonsPanelProps {
 //     buttons: {
@@ -21,40 +44,40 @@ const buttons = [
 //     }[]
 // }
 
-
 const ProfileButtonsPanel: FunctionComponent = () => {
-    return (
-        // <div className="w-fit max-w-[632px] mx-auto flex justify-center p-4">
-        //     <div className="flex flex-wrap gap-2">
-        //         {buttons.map((e, idx) => {
-        //             return (
-        //                 <div key={`${e}-${idx}`} className={`w-36 h-36 p-2 cursor-pointer ${e.color} rounded-md hover:bg-opacity-70 transition duration-300`}>
-        //                     <Link href={`/${e.href}`} className="text-sm text-center text-white border border-transparent">
-        //                         <div className="w-2/5 h-2/5 my-2 mx-auto flex justify-center">
-        //                             <FontAwesomeIcon className="h-full" icon={e.icon} />
-        //                         </div>
-        //                         <span className="text-base font-semibold">
-        //                             {e.name}
-        //                         </span>
-        //                     </Link>
-        //                 </div>
-        //             )
-        //         })}
-        //     </div>
-        // </div>
+  return (
+    // <div className="w-fit max-w-[632px] mx-auto flex justify-center p-4">
+    //     <div className="flex flex-wrap gap-2">
+    //         {buttons.map((e, idx) => {
+    //             return (
+    //                 <div key={`${e}-${idx}`} className={`w-36 h-36 p-2 cursor-pointer ${e.color} rounded-md hover:bg-opacity-70 transition duration-300`}>
+    //                     <Link href={`/${e.href}`} className="text-sm text-center text-white border border-transparent">
+    //                         <div className="w-2/5 h-2/5 my-2 mx-auto flex justify-center">
+    //                             <FontAwesomeIcon className="h-full" icon={e.icon} />
+    //                         </div>
+    //                         <span className="text-base font-semibold">
+    //                             {e.name}
+    //                         </span>
+    //                     </Link>
+    //                 </div>
+    //             )
+    //         })}
+    //     </div>
+    // </div>
 
-        <ul>
-            {buttons.map((e, idx) => {
-                return (
-                    <li key={idx} className="underline text-lg mb-2 hover:opacity-70">
-                        <Link href={e.href}>
-                            {e.name}
-                        </Link>
-                    </li>
-                )
-            })}
-        </ul>
-    )
-}
+    <ul>
+      {buttons.map((e, idx) => {
+        return (
+          <li
+            key={idx}
+            className="underline text-lg mb-2 hover:opacity-70"
+          >
+            <Link href={e.href}>{e.name}</Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
 
-export default ProfileButtonsPanel
+export default ProfileButtonsPanel;
--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -6,6 +6,7 @@ import { uploadImages } from "@/utils/formHelpers";
 import PhotoInput from "./PhotoInput";
 import ProfileButtonsPanel from "./ProfileButtonsPanel";
 import ProfileStats from "./ProfileStats";
+import AdsSwitch from "./AdsSwitch";
 
 const ProfileCard = () => {
   const { data: session } = useSession();
@@ -86,8 +87,9 @@ const ProfileCard = () => {
         <p className="font-bold text-2xl mb-3">{session!.user!.name}</p>
         <p className="italic mb-6 text-gray-600">{session!.user!.email}</p>
         <ProfileStats />
-        <div className="mx-auto flex justify-center max-[443px]:max-w-[11rem] min-[443px]:max-w-[20.7rem] min-[616px]:max-w-[30.5rem]">
+        <div className="mx-auto flex flex-col justify-center max-[443px]:max-w-[11rem] min-[443px]:max-w-[20.7rem] min-[616px]:max-w-[30.5rem]">
           <ProfileButtonsPanel />
+          <AdsSwitch />
         </div>
       </div>
     </div>
