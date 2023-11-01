091c94a8e3a9336dfc30d65b9aa3efb05658f2b5
Luis <simosa37@gmail.com>
2023-11-01 12:47:44 -0300

    fix profile stats & useTrackerAgent hook

--- src/components/ProfileStats.tsx
+++ src/components/ProfileStats.tsx
@@ -1,40 +1,48 @@
-import { useQuery } from '@tanstack/react-query'
-import React from 'react'
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
+import { useSession } from "next-auth/react";
 
 const ProfileStats = () => {
+  const { data: session } = useSession();
 
-    const { data, isLoading, error } = useQuery(["userStats"], async () => {
-        const response = await fetch("/api/user_agent_info?username=luissimosaarg@gmail.com", {
-            method: "GET",
-        })
+  const { data, isLoading, error } = useQuery(["userStats"], async () => {
+    const response = await fetch(
+      `/api/user_agent_info?username=${session?.user?.email}`,
+      {
+        method: "GET",
+      }
+    );
 
-        const data = await response.json()
-        return data
-    })
+    const data = await response.json();
+    return data;
+  });
 
-
-    if (isLoading) {
-        return (
-            <div>
-                <p className="italic text-gray-600">Cargando estadísticas...</p>
-            </div>
-        )
-    }
-
-    if (error) {
-        return (
-            <div>
-                <p className="italic text-gray-600">Error: {JSON.stringify(error)}</p>
-            </div>
-        )
-    }
-
+  if (isLoading) {
     return (
-        <div>
-            <p className="italic text-gray-600">Visitas a tu perfil: {data.visitsCount}</p>
-            <p className="italic mb-6 text-gray-600">Pacientes: {data.patientsCount}</p>
-        </div>
-    )
-}
+      <div>
+        <p className="italic text-gray-600">Cargando estadísticas...</p>
+      </div>
+    );
+  }
 
-export default ProfileStats
+  if (error) {
+    return (
+      <div>
+        <p className="italic text-gray-600">Error: {JSON.stringify(error)}</p>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <p className="italic text-gray-600">
+        Visitas a tu perfil: {data.visitsCount}
+      </p>
+      <p className="italic mb-6 text-gray-600">
+        Pacientes: {data.patientsCount}
+      </p>
+    </div>
+  );
+};
+
+export default ProfileStats;
